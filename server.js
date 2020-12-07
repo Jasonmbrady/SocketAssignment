6594fd1361789f4dbeb610be86207600c50fb3a0
Jason Brady <jasonmbrady42@gmail.com>
2020-12-07 04:23:12 -0800

    sockets functioning

--- server.js
+++ server.js
@@ -2,7 +2,14 @@ const express = require("express");
 const app = express();
 
 const server = app.listen(8000, () => console.log("Server running on port 8000"));
-const io = require("socket.io")(server);
+const io = require("socket.io")(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"]
+    }
+});
 io.on("connection", socket => {
     //place emitters here
+    socket.emit("Welcome", "Welcome to ChatApp");
+
 });
